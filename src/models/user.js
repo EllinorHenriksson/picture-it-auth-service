2690cc5106e8bc9187c4daa90df2268edbc4b27b
Ellinor Henriksson <eh224kr@student.lnu.se>
2022-04-07 14:47:46 +0200

    Implement user.js

---
+++ src/models/user.js
@@ -0,0 +1,97 @@
+/**
+ * Mongoose model User.
+ *
+ * @author Ellinor Henriksson
+ * @version 2.0.0
+ */
+
+import bcrypt from 'bcrypt'
+import mongoose from 'mongoose'
+import validator from 'validator'
+
+const { isEmail } = validator
+
+// Create a schema.
+const schema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, 'First name is required.'],
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Last name is required.'],
+    trim: true
+  },
+  email: {
+    type: String,
+    required: [true, 'Email address is required.'],
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: [isEmail, 'Please provide a valid email address.']
+  },
+  username: {
+    type: String,
+    required: [true, 'Username is required.'],
+    unique: true,
+    // - A valid username should start with an alphabet so, [A-Za-z].
+    // - All other characters can be alphabets, numbers or an underscore so, [A-Za-z0-9_-].
+    // - Since length constraint is 3-256 and we had already fixed the first character, so we give {2, 255}.
+    // - We use ^ and $ to specify the beginning and end of matching.
+    match: [/^[A-Za-z][A-Za-z0-9_-]{2,255}$/, 'Please provide a valid username.']
+  },
+  password: {
+    type: String,
+    minLength: [10, 'The password must be of minimum length 10 characters.'],
+    maxLength: [256, 'The password must be of maximum length 256 characters.'],
+    required: [true, 'Password is required.']
+  },
+  permissionLevel: Number
+}, {
+  timestamps: true,
+  toJSON: {
+    /**
+     * Performs a transformation of the resulting object to remove sensitive information.
+     *
+     * @param {object} doc - The mongoose document which is being converted.
+     * @param {object} ret - The plain object representation which has been converted.
+     */
+    transform: function (doc, ret) {
+      delete ret._id
+      delete ret.__v
+    },
+    virtuals: true // ensure virtual fields are serialized
+  }
+})
+
+schema.virtual('id').get(function () {
+  return this._id.toHexString()
+})
+
+// Salts and hashes password before save.
+schema.pre('save', async function () {
+  this.password = await bcrypt.hash(this.password, 10)
+})
+
+/**
+ * Authenticates a user.
+ *
+ * @param {string} username - ...
+ * @param {string} password - ...
+ * @returns {Promise<User>} ...
+ */
+schema.statics.authenticate = async function (username, password) {
+  const user = await this.findOne({ username })
+
+  // If no user found or password is wrong, throw an error.
+  if (!(await bcrypt.compare(password, user?.password))) {
+    throw new Error('Invalid credentials.')
+  }
+
+  // User found and password correct, return the user.
+  return user
+}
+
+// Create a model using the schema.
+export const User = mongoose.model('User', schema)
