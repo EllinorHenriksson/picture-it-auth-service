a021db38827c0f43c2a23d8bad6bff68702e85a0
Ellinor Henriksson <eh224kr@student.lnu.se>
2022-04-07 14:24:59 +0200

    Implement AuthController

---
+++ src/controllers/api/auth-controller.js
@@ -0,0 +1,96 @@
+/**
+ * Module for the AuthController.
+ *
+ * @author Ellinor Henriksson
+ * @version 1.0.0
+ */
+
+import jwt from 'jsonwebtoken'
+import createError from 'http-errors'
+import { User } from '../../models/user.js'
+
+/**
+ * Encapsulates a controller.
+ */
+export class AuthController {
+  /**
+   * Authenticates a user.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   */
+  async login (req, res, next) {
+    try {
+      const user = await User.authenticate(req.body.username, req.body.password)
+
+      const payload = {
+        sub: user.username,
+        given_name: user.firstName,
+        family_name: user.lastName,
+        email: user.email,
+        x_permission_level: user.permissionLevel
+      }
+
+      const privateKey = Buffer.from(process.env.ACCESS_TOKEN_SECRET, 'base64')
+
+      // Create access token.
+      const accessToken = jwt.sign(payload, privateKey, {
+        algorithm: 'RS256',
+        expiresIn: process.env.ACCESS_TOKEN_LIFE
+      })
+
+      res
+        .status(200)
+        .json({
+          access_token: accessToken
+        })
+    } catch (error) {
+      // Authentication failed.
+      const err = createError(401, 'Credentials invalid or not provided.')
+      err.cause = error
+
+      next(err)
+    }
+  }
+
+  /**
+   * Registers a user.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   */
+  async register (req, res, next) {
+    try {
+      const user = new User({
+        username: req.body.username,
+        password: req.body.password,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        permissionLevel: 1
+      })
+
+      await user.save()
+
+      res
+        .status(201)
+        .json({ id: user.id })
+    } catch (error) {
+      let err = error
+
+      if (err.code === 11000) {
+        // Duplicated keys.
+        err = createError(409, 'The username and/or email address is already registered.')
+        err.cause = error
+      } else if (error.name === 'ValidationError') {
+        // Validation error(s).
+        err = createError(400)
+        err.cause = error
+      }
+
+      next(err)
+    }
+  }
+}
